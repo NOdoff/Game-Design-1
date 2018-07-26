a1d3efc40379f1ba1f6f5e0c82d94eb4eaf2da42
Yelyzaveta Systaliuk <ysystali@e1z2r5p19.42.us.org>
2018-07-26 15:26:35 -0700

    new G1

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,9 @@
 gs = "main"
-board = [["1", " ", " "],
+board = [[" ", " ", " "],
          [" ", " ", " "],
         [" ", " ", " "]]
+X = "X"
+O = "O"
 turn = 1
 def main():
     global board
@@ -13,8 +15,22 @@ def main():
 def chooseTurn():
     if turn == 1:
         print("Player 1 it's your turn!")
+        # in_1 = input("Player 1 choose the place for your marker:\n")
     elif turn == 2:
         print("Player 2 it is your turn!")
-def store_int():
-    in_1 = input("Player 1 choose the place for your marker:")
+        # in_2 = input("Player 2 choose the place for your marker:\n")
+
+def marker():
+    print("Player 1, please choose marker X or O:")
+    choose_m = input()
+    if choose_m == "x" or choose_m == "X":
+        print("x")
+def store_info():
+    info = input("Player {} choose the place for your marker:\n".format(str(turn)))
+    # while 
+    #     if info == '7' an:
+    #         board[0][0]    
+    #     elif info == '':
+    #         board = 
+
 main()          
